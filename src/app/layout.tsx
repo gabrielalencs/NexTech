30a479bc5e6e123416da68186f35821cb83c914b
gabrielalencs <gabrieldealencar.dev@gmail.com>
2025-02-10 22:09:59 -0300

    feat: project base structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "NexTech - Eletrônicos e Tecnologia",
+    description: "Na NexTech, encontre os melhores eletrônicos e acessórios com qualidade e preços imbatíveis. Tecnologia que transforma o seu dia a dia!",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="pt-BR">
+            <body
+                className={inter.className}
+                cz-shortcut-listen="true"
+            >
+                {children}
+            </body>
+        </html>
+    );
+};
